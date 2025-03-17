34d6f035156be8ebd619d56b75518208d7b8ee55
Alen J Ninan <alen.j@captainfresh.in>
2025-03-17 17:05:40 +0530

    added page refresh and throttle function

--- src/app/details/[city]/page.jsx
+++ src/app/details/[city]/page.jsx
@@ -6,9 +6,8 @@ import {
   Navbar,
   News,
 } from "@/components/componentsIndex";
-import React, { use, useEffect, useState } from "react";
+import React, { use, useCallback, useEffect, useState } from "react";
 import styles from "../../home/home.module.scss";
-
 import {
   AirQualityCard,
   DailyForecastCard,
@@ -17,12 +16,40 @@ import {
 import { useFetchWeather } from "@/hooks/useFetchWeather";
 import { useFetchAirQuality } from "@/hooks/useFetchAirQuality";
 import { useFetchForecast } from "@/hooks/useFetchForecast";
+import { Box, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
+import RefreshIcon from "@mui/icons-material/Refresh";
+
+import Sharebtn from "./sharebtn";
+import { throttle } from "@/utils/helpers/throttle";
+import { useRouter } from "next/navigation";
 
 export default function Page({ params }) {
   const { city } = use(params);
-  const [loadingMessage, setLoadingMessage] = useState(`Searching for ${city} details ...`);
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [loadingMessage, setLoadingMessage] = useState(
+    `Searching for ${city} details ...`
+  );
   const [timeoutReached, setTimeoutReached] = useState(false);
 
+  const handleReload = useCallback(
+    throttle(() => {
+      setRefreshKey((prevKey) => prevKey + 1);
+      console.log("Window relaoded");
+    }, 5000),
+    []
+  );
+
+  const actions = [
+    { icon: <FileCopyIcon />, name: "Copy" },
+    { icon: <SaveIcon />, name: "Save" },
+    { icon: <PrintIcon />, name: "Print" },
+    { icon: <ShareIcon />, name: "Share" },
+    { icon: <RefreshIcon />, name: "Refresh Data", action: handleReload },
+  ];
   const {
     data: weatherData,
     loading: weatherDataLoading,
@@ -42,7 +69,12 @@ export default function Page({ params }) {
   } = useFetchForecast(city);
 
   const isLoading =
-    weatherDataLoading || airQualityDataLoading || forecastDataLoading;
+    !weatherData ||
+    !airQualityData ||
+    !forecastData ||
+    weatherDataLoading ||
+    airQualityDataLoading ||
+    forecastDataLoading;
 
   const isError =
     !isLoading &&
@@ -57,7 +89,7 @@ export default function Page({ params }) {
   }, [city]);
 
   return (
-    <div className={styles.home}>
+    <div className={styles.home} key={refreshKey}>
       <Navbar />
 
       {isLoading && !timeoutReached ? (
@@ -66,7 +98,9 @@ export default function Page({ params }) {
         </div>
       ) : isError ? (
         <div className={styles.errorContainer}>
-          <p>No details found for {city} or {city} does not exist</p>
+          <p>
+            No details found for {city} or {city} does not exist
+          </p>
         </div>
       ) : (
         <div className={styles.weatherContainer}>
@@ -75,7 +109,9 @@ export default function Page({ params }) {
             <UvIndexCard uvIndex={weatherData.current.uv} />
             {airQualityData && <AirQualityCard aqData={airQualityData} />}
             {forecastData && (
-              <DailyForecastCard forecastData={forecastData.forecast.forecastday[0].hour} />
+              <DailyForecastCard
+                forecastData={forecastData.forecast.forecastday[0].hour}
+              />
             )}
           </div>
 
@@ -89,6 +125,22 @@ export default function Page({ params }) {
           </div>
         </div>
       )}
+      <div>
+        <SpeedDial
+          ariaLabel="SpeedDial"
+          sx={{ position: "fixed", bottom: 16, right: 16 }}
+          icon={<SpeedDialIcon />}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+              onClick={action.action}
+            />
+          ))}
+        </SpeedDial>
+      </div>
     </div>
   );
 }
