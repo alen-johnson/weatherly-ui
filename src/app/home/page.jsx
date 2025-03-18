b88af2563298f5aa665f34e2351b0658872de75c
Alen J Ninan <alen.j@captainfresh.in>
2025-03-18 23:34:20 +0530

    added visibilty card

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -58,7 +58,7 @@ export default function Home() {
                 strokeLinecap="round"
                 strokeMiterlimit="10"
                 strokeWidth="1.5"
-                stroke="#292D32"
+                stroke="#ffff"
                 d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008"
               ></path>
             </svg>
